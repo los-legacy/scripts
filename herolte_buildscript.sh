cfde547d94b493e572cc71469958706bebb59ab1
BenLue <BenJule@users.noreply.github.com>
2020-03-09 00:59:53 +0100

    Create herolte_buildscript.sh

---
+++ herolte_buildscript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/exodusnick/LineageOS/17.1_LineageOS/
+make clean
+export PATH=~/bin:$PATH
+export USE_CCACHE=1
+ccache -M 50G
+export CCACHE_COMPRESS=1
+repo sync --no-clone-bundle --force-sync
+source build/envsetup.sh && breakfast herolte && time brunch herolte
