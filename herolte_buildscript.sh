96f3bcd644fcce6fcf6d0247a5266882f7298d1e
BenJule <benlue@s3root.ovh>
2020-03-09 19:18:05 +0100

    Remove ccache for hero builds

--- herolte_buildscript.sh
+++ herolte_buildscript.sh
@@ -3,8 +3,5 @@ set -x
 cd /home/exodusnick/LineageOS/17.1_LineageOS/
 make clean
 export PATH=~/bin:$PATH
-export USE_CCACHE=1
-ccache -M 50G
-export CCACHE_COMPRESS=1
 repo sync --no-clone-bundle --force-sync
 source build/envsetup.sh && breakfast herolte && time brunch herolte
